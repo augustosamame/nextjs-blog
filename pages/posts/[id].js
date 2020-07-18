1d8e0af2ae1809d6cea23bcb57e6c53c57a8933d
Augusto Samamé <augustosamame@gmail.com>
2020-07-17 21:59:49 -0500

    fix typo in import

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout'
-import Head from 'next/Head'
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 
